0f164cb817a006f36054d22a2351e2943fcf13f7
Tonny <corner.store59@gmail.com>
2023-11-21 17:49:50 -0600

    [ADD] Domain in department [FIX] Create purchase order

--- purchase_custom/models/purchase_order.py
+++ purchase_custom/models/purchase_order.py
@@ -6,7 +6,8 @@ class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
     employee_id = fields.Many2one('hr.employee', compute='_compute_employee_id', compute_sudo=True)
-    department_id = fields.Many2one('hr.department')
+    employee_departament_id = fields.Many2one(related="employee_id.department_id")
+    department_id = fields.Many2one('hr.department', domain='[("id", "=", employee_departament_id)]')
     is_acquisition = fields.Boolean(related='employee_id.department_id.is_acquisition')
     partner_id = fields.Many2one(required=False)
     request_user_id = fields.Many2one('res.partner')
@@ -28,12 +29,16 @@ class PurchaseOrder(models.Model):
     @api.model_create_multi
     def create(self, vals_list):
         for val in vals_list:
+            if not val['user_id']:
+                val['partner_id'] = self.env.user.partner_id.id
+                val['request_user_id'] = self.env.user.partner_id.id
             employee_id = self.env['hr.employee'].search([('user_id', '=', val['user_id'])])
             if not employee_id:
                 raise UserError(_('There is no employee related to this user.'))
             if not val['partner_id']:
                 val['partner_id'] = self.env.user.partner_id.id
-            val['employee_id'] = employee_id.id
+            if len(employee_id) == 1 and val['user_id']:
+                val['employee_id'] = employee_id.id
             val['request_user_id'] = self.env.user.partner_id.id
         res = super().create(vals_list)
         return res
